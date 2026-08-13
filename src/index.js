eb3d751cd7abd26affff22328e541831bde691ed
Jake Januszanis <jakejanuszanis@Jakes-MacBook-Air.local>
2026-08-12 19:51:20 -0700

    Intergrate GitHub PR data into description pipeline

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import "dotenv/config";
-import { samplePR } from "../tests/fixtures/samplePR.js";
+import { getPullRequest } from "./getPullRequest.js";
 import { generateDescription } from "./descriptionGenerator.js";
 import { formatDescription } from "./formatDescription.js";
 
-const description = await generateDescription(samplePR);
+const pullRequest = await getPullRequest(
+  process.env.GITHUB_OWNER,
+  process.env.GITHUB_REPO,
+  Number(process.env.GITHUB_PR_NUMBER)
+);
+
+const description = await generateDescription(pullRequest);
 const formattedDescription = formatDescription(description);
 
 console.log(formattedDescription);

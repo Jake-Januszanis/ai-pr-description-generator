b5dcfa0c7e3418b995ef3422ca5bb0cde8ab39ad
Jake Januszanis <jakejanuszanis@Jakes-MacBook-Air.local>
2026-08-17 10:24:11 -0700

    Added function to get user preferences

---
+++ src/userPreferences.js
@@ -0,0 +1,7 @@
+export function getUserPreferences(user) {
+  return {
+    theme: user.theme ?? "light",
+    notifications: user.notifications ?? true,
+    language: user.language ?? "en"
+  };
+}
